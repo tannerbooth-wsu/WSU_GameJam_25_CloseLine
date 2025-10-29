b90c4ed83734604c8f7c19a9869d513a441ca6b6
nayrbnamknirb <bryanbrinkman15@gmail.com>
2025-10-29 16:33:12 -0600

    Fixes to get it to build

--- WSU_GameJam_25_CloseLine/Assets/Scripts/EnemyMovement.cs
+++ WSU_GameJam_25_CloseLine/Assets/Scripts/EnemyMovement.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Diagnostics;
 using TMPro;
-using UnityEditor.Build.Reporting;
+//using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.SocialPlatforms.Impl;
